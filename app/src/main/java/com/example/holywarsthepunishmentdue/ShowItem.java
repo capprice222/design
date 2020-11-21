3be002eb42538e5887401a18df31d98fa8051074
Yaroslav.Zapolskiy <zapolechka5@gmail.com>
2020-11-21 21:14:07 +0700

    killing for religion? something i don't understand

---
+++ app/src/main/java/com/example/holywarsthepunishmentdue/ShowItem.java
@@ -0,0 +1,14 @@
+package com.example.holywarsthepunishmentdue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_item);
+    }
+}
